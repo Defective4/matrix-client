edfad0100ed6f0c6201686d59f660f4043c80332
Defective <def3ctive4@gmail.com>
2026-09-25 15:40:11 +0200

    Add a method to open private chats with users

--- src/main/java/io/github/defective4/matrix/client/matrix/entity/user/User.java
+++ src/main/java/io/github/defective4/matrix/client/matrix/entity/user/User.java
@@ -1,10 +1,15 @@
 package io.github.defective4.matrix.client.matrix.entity.user;
 
 import java.io.IOException;
+import java.util.List;
 
+import com.google.gson.JsonObject;
+
+import io.github.defective4.matrix.client.http.HTTPMethod;
 import io.github.defective4.matrix.client.matrix.MatrixClient;
 import io.github.defective4.matrix.client.matrix.entity.Entity;
 import io.github.defective4.matrix.client.matrix.entity.Room;
+import io.github.defective4.matrix.client.matrix.model.request.CreateRoomRequest;
 
 public class User extends Entity {
 
@@ -13,6 +18,12 @@ public class User extends Entity {
     public User(MatrixClient client, String id) {
         super(client);
         this.id = id;
+    }
+
+    public Room createPrivateChat() throws IOException {
+        return new Room(client, client.makeRequest("/createRoom",
+                new CreateRoomRequest(Room.Visibility.PRIVATE.name().toLowerCase(), null, null, List.of(id), true),
+                JsonObject.class, HTTPMethod.POST).get("room_id").getAsString());
     }
 
     public String getId() {
